e00e934f0041d04b0651691224e7adf7c47d3eef
louisbl <louis@beltramo.me>
2016-02-04 00:42:02 +0100

    Add note model

---
+++ app/src/main/java/fr/louisbl/remember/notes/Note.java
@@ -0,0 +1,17 @@
+package fr.louisbl.remember.notes;
+
+public class Note {
+    String id;
+    String title;
+    String description;
+
+    public Note() {
+
+    }
+
+    public Note(String id, String title, String description) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+    }
+}
