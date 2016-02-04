fdfcc72932ec796e755884933414ba07a0f76eb8
louisbl <louis@beltramo.me>
2016-02-04 01:00:59 +0100

    Add fragment with recycler view

---
+++ app/src/main/java/fr/louisbl/remember/notes/ListNotes.java
@@ -0,0 +1,34 @@
+package fr.louisbl.remember.notes;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import fr.louisbl.remember.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ListNotes extends Fragment {
+
+
+    public ListNotes() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.list_notes, container, false);
+
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.list_notes);
+
+        return view;
+    }
+
+}
